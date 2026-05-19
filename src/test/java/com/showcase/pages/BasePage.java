319ce9ceae5423a659c63f5e458c5349c3a6a229
Nahuel <lucas56nahuel@gmail.com>
2026-05-19 13:07:45 -0300

    Reformat

--- src/test/java/com/showcase/pages/BasePage.java
+++ src/test/java/com/showcase/pages/BasePage.java
@@ -9,36 +9,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
 
-    protected final WebDriver driver;
+  protected final WebDriver driver;
 
-    private final WebDriverWait wait;
+  private final WebDriverWait wait;
 
-    protected BasePage(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
+  protected BasePage(WebDriver driver) {
+    this.driver = driver;
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+  }
 
-    protected void click(By locator) {
-        findClickable(locator).click();
-    }
+  protected void click(By locator) {
+    findClickable(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        findVisible(locator).sendKeys(text);
-    }
+  protected void type(By locator, String text) {
+    findVisible(locator).sendKeys(text);
+  }
 
-    protected String getText(By locator) {
-        return findVisible(locator).getText();
-    }
+  protected String getText(By locator) {
+    return findVisible(locator).getText();
+  }
 
-    protected boolean isDisplayed(By locator) {
-        return findVisible(locator).isDisplayed();
-    }
+  protected boolean isDisplayed(By locator) {
+    return findVisible(locator).isDisplayed();
+  }
 
-    private WebElement findVisible(By locator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
+  private WebElement findVisible(By locator) {
+    return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+  }
 
-    private WebElement findClickable(By locator) {
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
+  private WebElement findClickable(By locator) {
+    return wait.until(ExpectedConditions.elementToBeClickable(locator));
+  }
 }
--- src/test/java/com/showcase/tests/LoginTest.java
+++ src/test/java/com/showcase/tests/LoginTest.java
@@ -7,20 +7,20 @@ import org.junit.jupiter.api.Test;
 
 class LoginTest extends BaseTest {
 
-    @Test
-    void shouldLoginWithValidCredentials() {
-        LoginPage loginPage = new LoginPage(driver);
+  @Test
+  void shouldLoginWithValidCredentials() {
+    LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.open();
+    loginPage.open();
 
-        assertTrue(loginPage.isLoaded(), "Login page should be loaded");
+    assertTrue(loginPage.isLoaded(), "Login page should be loaded");
 
-        loginPage.enterUsername("tomsmith");
-        loginPage.enterPassword("SuperSecretPassword!");
-        loginPage.submit();
+    loginPage.enterUsername("tomsmith");
+    loginPage.enterPassword("SuperSecretPassword!");
+    loginPage.submit();
 
-        assertTrue(
-            loginPage.getFlashMessage().contains("You logged into a secure area!"),
-            "Success login message should be displayed");
-    }
+    assertTrue(
+        loginPage.getFlashMessage().contains("You logged into a secure area!"),
+        "Success login message should be displayed");
+  }
 }
