db202c8f4980f348bd6218199be263dbcd1c5aa7
Nahuel <lucas56nahuel@gmail.com>
2026-05-22 17:51:50 -0300

    Add basic framework logging

--- src/test/java/com/showcase/tests/BaseTest.java
+++ src/test/java/com/showcase/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package com.showcase.tests;
 
 import com.showcase.framework.config.ConfigReader;
+import com.showcase.framework.driver.BrowserType;
 import com.showcase.framework.driver.DriverFactory;
 import com.showcase.framework.utils.ScreenshotUtils;
 import org.junit.jupiter.api.AfterEach;
@@ -8,8 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 abstract class BaseTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
 
     protected WebDriver driver;
 
@@ -17,7 +22,9 @@ abstract class BaseTest {
     private final TestExecutionExceptionHandler screenshotOnFailure = (context, throwable) -> {
         if (driver != null) {
             try {
-                ScreenshotUtils.takeScreenshot(driver, context.getRequiredTestMethod().getName());
+                String testName = context.getRequiredTestMethod().getName();
+                LOGGER.warn("Test failed. Capturing screenshot for: {}", testName);
+                ScreenshotUtils.takeScreenshot(driver, testName);
             } catch (RuntimeException screenshotException) {
                 throwable.addSuppressed(screenshotException);
             }
@@ -28,12 +35,15 @@ abstract class BaseTest {
 
     @BeforeEach
     void setUp() {
-        driver = DriverFactory.createDriver(ConfigReader.getBrowserType());
+        BrowserType browserType = ConfigReader.getBrowserType();
+        LOGGER.info("Starting browser: {}", browserType);
+        driver = DriverFactory.createDriver(browserType);
   }
 
   @AfterEach
   void tearDown() {
     if (driver != null) {
+      LOGGER.info("Closing browser");
       driver.quit();
     }
   }
