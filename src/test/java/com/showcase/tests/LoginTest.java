3b4890371c4610c494e939f9bec5dd43310efbcd
Nahuel <lucas56nahuel@gmail.com>
2026-05-27 21:20:32 -0300

    Add negative login scenarios

--- src/test/java/com/showcase/tests/LoginTest.java
+++ src/test/java/com/showcase/tests/LoginTest.java
@@ -9,11 +9,7 @@ class LoginTest extends BaseTest {
 
   @Test
   void shouldLoginWithValidCredentials() {
-    LoginPage loginPage = new LoginPage(driver);
-
-    loginPage.open();
-
-    assertTrue(loginPage.isLoaded(), "Login page should be loaded");
+    LoginPage loginPage = openLoginPage();
 
     loginPage.enterUsername("tomsmith");
     loginPage.enterPassword("SuperSecretPassword!");
@@ -22,5 +18,38 @@ class LoginTest extends BaseTest {
     assertTrue(
         loginPage.getFlashMessage().contains("You logged into a secure area!"),
         "Success login message should be displayed");
+  }
+
+  @Test
+  void shouldShowErrorMessageWithInvalidUsername() {
+    LoginPage loginPage = openLoginPage();
+
+    loginPage.enterUsername("invalid-user");
+    loginPage.enterPassword("SuperSecretPassword!");
+    loginPage.submit();
+
+    assertTrue(
+        loginPage.getFlashMessage().contains("Your username is invalid!"),
+        "Invalid username error message should be displayed");
+  }
+
+  @Test
+  void shouldShowErrorMessageWithInvalidPassword() {
+    LoginPage loginPage = openLoginPage();
+
+    loginPage.enterUsername("tomsmith");
+    loginPage.enterPassword("invalid-password");
+    loginPage.submit();
+
+    assertTrue(
+        loginPage.getFlashMessage().contains("Your password is invalid!"),
+        "Invalid password error message should be displayed");
+  }
+
+  private LoginPage openLoginPage() {
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.open();
+    assertTrue(loginPage.isLoaded(), "Login page should be loaded");
+    return loginPage;
   }
 }
